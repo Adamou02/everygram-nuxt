f47d31e6007723c2763e29ce35580e1608ebab87
boyinlee <bo-yin.lee@houzz.com>
2024-06-06 00:49:13 +0800

    fix: update styles

--- pages/trips.vue
+++ pages/trips.vue
@@ -1,39 +1,43 @@
 <template>
     <div class="flex flex-column gap-5">
-        <SectionTitleBar v-if="isFetchingTrips || trips.length > 0">
-            <div class="text-color-light">
-                {{ $t('INFO_TRIP_NUM', { num: trips.length }, trips.length) }}
+        <template v-if="isFetchingTrips || trips.length > 0">
+            <SectionTitleBar>
+                <div class="text-color-light">
+                    {{
+                        $t('INFO_TRIP_NUM', { num: trips.length }, trips.length)
+                    }}
+                </div>
+                <PrimeButton
+                    severity="secondary"
+                    rounded
+                    :label="$t('ACTION_CREATE_TRIP')"
+                    icon="pi pi-plus"
+                    @click="onCreateTrip"
+                />
+            </SectionTitleBar>
+            <div class="grid">
+                <div
+                    v-if="isFetchingTrips"
+                    v-for="i in 6"
+                    class="col-12 md:col-6 lg:col-3"
+                    :key="i"
+                >
+                    <TripCardSkeleton />
+                </div>
+                <div
+                    v-else
+                    v-for="trip in trips"
+                    class="col-12 md:col-6 lg:col-3"
+                    :key="trip.id"
+                >
+                    <NuxtLink :to="`/trip/${trip.id}`">
+                        <TripCard :key="trip.id" :trip="trip" />
+                    </NuxtLink>
+                </div>
             </div>
-            <PrimeButton
-                severity="secondary"
-                rounded
-                :label="$t('ACTION_CREATE_TRIP')"
-                icon="pi pi-plus"
-                @click="onCreateTrip"
-            />
-        </SectionTitleBar>
-        <div class="grid">
-            <div
-                v-if="isFetchingTrips"
-                v-for="i in 6"
-                class="col-12 md:col-6 lg:col-3"
-                :key="i"
-            >
-                <TripCardSkeleton />
-            </div>
-            <div
-                v-else
-                v-for="trip in trips"
-                class="col-12 md:col-6 lg:col-3"
-                :key="trip.id"
-            >
-                <NuxtLink :to="`/trip/${trip.id}`">
-                    <TripCard :key="trip.id" :trip="trip" />
-                </NuxtLink>
-            </div>
-        </div>
+        </template>
         <EmptyState
-            v-if="!isFetchingTrips && trips.length === 0"
+            v-else
             :title="$t('INFO_NO_USER_TRIPS')"
             :description="$t('INFO_NO_USER_TRIPS_DESC')"
             image-src="/image/empty-trip.jpg"
