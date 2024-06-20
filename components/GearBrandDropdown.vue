e89c1e1640a28b264c7ebb27a15362fc3c904a0e
boyinlee <bo-yin.lee@houzz.com>
2024-06-20 20:40:48 +0800

    feat: include custom brands in gear brand dropdown

--- components/GearBrandDropdown.vue
+++ components/GearBrandDropdown.vue
@@ -1,19 +1,48 @@
 <template>
     <PrimeDropdown
         v-model="value"
-        :options="
-            constants.GEAR_BRAND_KEYS.map((brand) => ({
-                value: brand,
-                label: constants.GEAR_BRANDS[brand].name,
-            }))
-        "
+        :options="options"
         editable
         optionValue="value"
         optionLabel="label"
+        :optionDisabled="(option) => !option.value"
         :virtualScrollerOptions="{ itemSize: 40 }"
     />
 </template>
 
 <script setup lang="ts">
 const value = defineModel<string>({ required: true });
+const userGearsStore = useUserGearsStore();
+const { gears } = storeToRefs(userGearsStore);
+const i18n = useI18n();
+const customBrandOptions = computed(() => {
+    return _uniq(
+        gears.value
+            .map((gear) => gear.brand?.custom)
+            .filter((custom) => custom),
+    )
+        .sort()
+        .map((custom) => ({
+            value: custom,
+            label: custom,
+        }));
+});
+const options = computed(() => {
+    return [
+        ...constants.GEAR_BRAND_KEYS.map((brand) => ({
+            value: brand,
+            label: constants.GEAR_BRANDS[brand].name,
+        })),
+        ...(customBrandOptions.value.length
+            ? [
+                  {
+                      label: i18n.t('LABEL_CUSTOM_BRAND'),
+                      value: null,
+                      disabled: true,
+                  },
+                  ...customBrandOptions.value,
+              ]
+            : []),
+    ];
+});
 </script>
