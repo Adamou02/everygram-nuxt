f222ce029ff4a86f4f518f3b30360c3857306e46
BoYin Lee <leeboyin@BoYins-MacBook-Pro.local>
2025-06-30 00:31:12 +0900

    fix: anniversary count logic

--- composables/use-display-gears.ts
+++ composables/use-display-gears.ts
@@ -63,21 +63,30 @@ export default function () {
 
     // anniversary gears
     const UPCOMING_DAYS = 7;
+    const dateNow = new Date();
+    const todayDateOnly = new Date(
+        dateNow.getFullYear(),
+        dateNow.getMonth(),
+        dateNow.getDate(),
+    );
     const getNextGearAnniversaryDate = (acquiredDate: Date): Date => {
-        const today = new Date();
         const anniversaryDate = new Date(acquiredDate);
-        anniversaryDate.setFullYear(today.getFullYear());
+        anniversaryDate.setFullYear(todayDateOnly.getFullYear());
+        const anniversaryDateOnly = new Date(
+            anniversaryDate.getFullYear(),
+            anniversaryDate.getMonth(),
+            anniversaryDate.getDate(),
+        );
 
-        // If the anniversary date is in the past, set it to next year
-        if (anniversaryDate <= today) {
-            anniversaryDate.setFullYear(today.getFullYear() + 1);
+        // If the anniversary date is in the past (date only), set it to next year
+        if (anniversaryDateOnly <= todayDateOnly) {
+            anniversaryDateOnly.setFullYear(todayDateOnly.getFullYear() + 1);
         }
 
-        return anniversaryDate;
+        return anniversaryDateOnly;
     };
 
     const anniversaryGearList = computed<GearWithAnniversary[]>(() => {
-        const today = new Date();
         const results: GearWithAnniversary[] = [];
 
         visibleGears.value.forEach((gear) => {
@@ -88,12 +97,13 @@ export default function () {
             // find gears that acquired date is the same as today, but acquired date's year is in the past
             const acquiredDate = new Date(gear.acquiredDate);
             const isAnniversary =
-                acquiredDate.getDate() === today.getDate() &&
-                acquiredDate.getMonth() === today.getMonth() &&
-                acquiredDate.getFullYear() < today.getFullYear();
+                acquiredDate.getDate() === todayDateOnly.getDate() &&
+                acquiredDate.getMonth() === todayDateOnly.getMonth() &&
+                acquiredDate.getFullYear() < todayDateOnly.getFullYear();
             if (isAnniversary) {
-                const age = today.getFullYear() - acquiredDate.getFullYear();
-                const anniversaryDate = today;
+                const age =
+                    todayDateOnly.getFullYear() - acquiredDate.getFullYear();
+                const anniversaryDate = todayDateOnly;
                 const daysUntilAnniversary = 0;
                 results.push({
                     ...gear,
@@ -110,7 +120,6 @@ export default function () {
     });
 
     const upcomingAnniversaryGears = computed<GearWithAnniversary[]>(() => {
-        const today = new Date();
         const results: GearWithAnniversary[] = [];
         // Change from anniversaryGearList.value to visibleGears.value
         visibleGears.value.forEach((gear) => {
@@ -118,37 +127,29 @@ export default function () {
                 return; // Skip gears without an acquired date
             }
             const acquiredDate = new Date(gear.acquiredDate);
-            const nextAnniversaryDate =
+            const anniversaryDateOnly =
                 getNextGearAnniversaryDate(acquiredDate);
 
             // find gears that have an anniversary date in the coming days
-            const maxUpcomingDay = new Date(today);
-            maxUpcomingDay.setDate(today.getDate() + UPCOMING_DAYS);
+            const maxUpcomingDay = new Date(todayDateOnly);
+            maxUpcomingDay.setDate(todayDateOnly.getDate() + UPCOMING_DAYS);
             const isAnniversaryComing =
-                nextAnniversaryDate > today &&
-                nextAnniversaryDate <= maxUpcomingDay;
+                anniversaryDateOnly > todayDateOnly &&
+                anniversaryDateOnly <= maxUpcomingDay;
             if (isAnniversaryComing) {
-                const start = new Date(
-                    today.getFullYear(),
-                    today.getMonth(),
-                    today.getDate(),
-                );
-                const end = new Date(
-                    nextAnniversaryDate.getFullYear(),
-                    nextAnniversaryDate.getMonth(),
-                    nextAnniversaryDate.getDate(),
-                );
+                const start = todayDateOnly;
+                const end = anniversaryDateOnly;
                 const daysUntilAnniversary = Math.round(
                     (end.getTime() - start.getTime()) / (1000 * 60 * 60 * 24),
                 );
                 const age =
-                    nextAnniversaryDate.getFullYear() -
+                    anniversaryDateOnly.getFullYear() -
                     acquiredDate.getFullYear();
                 results.push({
                     ...gear,
                     age,
                     daysUntilAnniversary,
-                    anniversaryDate: nextAnniversaryDate,
+                    anniversaryDate: anniversaryDateOnly,
                 });
             }
         });
