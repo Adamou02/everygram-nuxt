fb1bf5c667d3191862c89f6324c10e4eb4e6a6ca
boyin.lee <leeboyin945@gmail.com>
2025-08-21 17:35:24 +0800

    fix: extract firebase config to .env

--- plugins/firebase.ts
+++ plugins/firebase.ts
@@ -1,11 +1,23 @@
 import 'firebase/auth';
-import firebaseConfig from '~/firebase.config.json';
 import { initializeApp } from 'firebase/app';
 export default defineNuxtPlugin({
     name: 'firebase.client',
     async setup(nuxtApp) {
         // this is the equivalent of a normal functional plugin
         // initialize Firebase app
+        const config = useRuntimeConfig();
+        const firebaseConfig = {
+            apiKey: config.public.FIREBASE_API_KEY as string,
+            authDomain: config.public.FIREBASE_AUTH_DOMAIN as string,
+            databaseURL: config.public.FIREBASE_DATABASE_URL as string,
+            projectId: config.public.FIREBASE_PROJECT_ID as string,
+            storageBucket: config.public.FIREBASE_STORAGE_BUCKET as string,
+            messagingSenderId: config.public
+                .FIREBASE_MESSAGING_SENDER_ID as string,
+            appId: config.public.FIREBASE_APP_ID as string,
+            measurementId: config.public.FIREBASE_MEASUREMENT_ID as string,
+        };
+
         initializeApp(firebaseConfig);
     },
     hooks: {
